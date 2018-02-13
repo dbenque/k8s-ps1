4dadc0e57c00cdf2a05dfdde9c40e1d7b9a03514
David BENQUE <dbenque@amadeus.com>
2018-02-13 20:36:42 +0100

    fix case with only one context

--- kcontext.sh
+++ kcontext.sh
@@ -1,2 +1,4 @@
-KC="";for c in `ls -p ~/.kube | grep -v '/'`; do KC=~/.kube/$c:$KC; done;
-export KUBECONFIG=$KC
+#!/bin/bash
+
+KC="";for c in `ls -p ~/.kube 2>/dev/null | grep -v '/'`; do KC=~/.kube/$c:$KC; done;
+export KUBECONFIG=$(echo $KC | sed -r 's/(.*)(:)+/\1/g')
